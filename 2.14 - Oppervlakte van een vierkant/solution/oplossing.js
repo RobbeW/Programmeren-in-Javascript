dcf7e95666e2221fc0d51b507bd71c7e1ee85bb5
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2024-11-03 10:25:54 +0100

    Update oplossing.js
    missing semicolon

--- 2.14 - Oppervlakte van een vierkant/solution/oplossing.js
+++ 2.14 - Oppervlakte van een vierkant/solution/oplossing.js
@@ -1,4 +1,4 @@
 var zijde = 16.5; 
 var omtrek = zijde * 4;
 var opp = zijde * zijde; 
-console.log("De omtrek van het vierkant is", omtrek, "cm en de oppervlakte is", opp, "cm²")
+console.log("De omtrek van het vierkant is", omtrek, "cm en de oppervlakte is", opp, "cm²");
