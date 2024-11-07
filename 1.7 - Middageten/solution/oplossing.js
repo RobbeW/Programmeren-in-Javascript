5a91028137da015978a9d12e06b20d7c54e216b2
Exy Sabbe <exy.sabbe@sintlievenscollege.be>
2024-11-07 13:36:49 +0100

    Fixed oplossing.js

--- 1.7 - Middageten/solution/oplossing.js
+++ 1.7 - Middageten/solution/oplossing.js
@@ -4,7 +4,7 @@ var bijgerecht = 'puree';
 var dessert = 'pudding';
 var populariteit = '7.8/10';
 
-console.log('Op ' + datum + ' waren er in de cafetaria ' + hoofdgerecht + ' met ' + puree + ' te vinden.');
-console.log('Als dessert kon je ' + pudding + ' krijgen.');
+console.log('Op ' + datum + ' waren er in de cafetaria ' + hoofdgerecht + ' met ' + bijgerecht + ' te vinden.');
+console.log('Als dessert kon je ' + dessert + ' krijgen.');
 console.log('De studenten gaven deze maaltijd gemiddeld een ' + populariteit + '.');
 
