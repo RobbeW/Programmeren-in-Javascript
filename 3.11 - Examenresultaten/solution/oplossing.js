59bd3c18b4b224114c8ac43265d7293ddbae582d
Exy Sabbe <exy.sabbe@sintlievenscollege.be>
2025-02-14 12:54:07 +0100

    Fixed solution for test exercise

--- 3.11 - Examenresultaten/solution/oplossing.js
+++ 3.11 - Examenresultaten/solution/oplossing.js
@@ -4,11 +4,11 @@ var resultaat = 8
 // Controleer het resultaat
 if (resultaat < 0 || resultaat > 122) {
     console.log("Ongeldig cijfer.");
-} else if (leeftijd < 10) {
+} else if (resultaat < 10) {
     console.log("Helaas, je bent gezakt.");
-} else if (leeftijd <= 13) {
+} else if (resultaat <= 13) {
     console.log("Je bent geslaagd, maar het kan beter.");
-} else if (leeftijd < 15) {
+} else if (resultaat < 15) {
     console.log("Proficiat, je bent geslaagd met onderscheiding.");
 } else {
     console.log("Uitmuntend, je bent met grote onderscheiding geslaagd!");
